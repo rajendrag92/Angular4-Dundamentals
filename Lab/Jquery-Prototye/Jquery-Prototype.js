22dae93e0a5b6f6ca89359d17aa05117d7e533bb
Rajendra <rgupta@elevate.com>
2018-01-02 16:53:45 -0600

    Updated Object prototype

--- Lab/Jquery-Prototye/Jquery-Prototype.js
+++ Lab/Jquery-Prototye/Jquery-Prototype.js
@@ -1,5 +1,12 @@
 'use strict'
 
+
+/* Function's prototype: A funtion prototupe is a object 
+instance that will become a prototype for all objects created
+using this function as constructor */
+
+/* Object's Prototype: A object prototype is the object
+ instance from which object is inherited*/
  function Animals(_voice){
      console.log(_voice)
      this.voice = _voice;
@@ -14,11 +21,12 @@
      
       this.name = _name;
       this.color = _color;
-
  }
+ 
 
  Cat.prototype = Object.create(Animals.prototype);
  Cat.prototype.constructor = 'Cat';
+ Cat.prototype.age = 5;
  Cat.prototype.whatCatIs = function(){
       console.log(this.name + " is a "+ this.color +" color car!!");
  };
@@ -32,3 +40,22 @@
 
  rocky.whatCatIs();
  rocky.speaks();
+ 
+ Object.defineProperty(rocky,"nameLenght", {
+    get:function(){
+        console.log(this.name);
+        //return this.name.toString().length;
+    }
+})
+
+ console.log("Fluffy age is => " + fluffy.age);
+ console.log("Rocky age is => " + rocky.age);
+ 
+ console.log("Updating Rocky object age on object __proto__.age = 60 will update Fluffy age too"  );
+ 
+ rocky.__proto__.age = 60
+
+ console.log("Fluffy age is => " + fluffy.age);
+ console.log("Rocky age is => " + rocky.age);
+ 
+ console.log("length of name is => " + rocky['name'])

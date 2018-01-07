d76a5fb21205e3c5c3b7b2cbefbe7aa34a68fcd3
Rajendra <rgupta@elevate.com>
2018-01-07 00:48:39 -0600

    test

---
+++ Lab/OAuth/socialAggregator/routes/auth.js
@@ -0,0 +1,17 @@
+var passport = require('passport');
+var express = require('express');
+var router = express.Router();
+
+router.route('/google/callback').get(passport.authenticate('google',{
+
+        successRedirect:'/users/',
+        failure:'/error/'
+}));
+
+
+router.route('/google').get(passport.authenticate('google',{
+    scope:['https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/userinfo.email']
+}));
+
+module.exports = router;
